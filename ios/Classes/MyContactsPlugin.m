5d42fa3f4b6d9672f52192e1398614902b2929c5
lambasoft <lambasoftwares@gmail.com>
2019-02-10 18:25:52 +0200

    Initial Commit

---
+++ ios/Classes/MyContactsPlugin.m
@@ -0,0 +1,20 @@
+#import "MyContactsPlugin.h"
+
+@implementation MyContactsPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"my_contacts"
+            binaryMessenger:[registrar messenger]];
+  MyContactsPlugin* instance = [[MyContactsPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
